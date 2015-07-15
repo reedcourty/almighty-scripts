8b5c52f8f8ff896f331858ed2ffdbde7d2b2ed3d
reedcourty <reedcourty@gmail.com>
2015-07-15 20:44:17 +0200

    Known networks are at start of the list.

--- ap_logger/ap_logger.py
+++ ap_logger/ap_logger.py
@@ -19,7 +19,17 @@ if __name__ == '__main__':
 
     while True:
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "SSID.{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d"))), "a") as ssid_log:
-            line = get_line(netshwlan.get_networks(mode="bssid"))
+            networks = netshwlan.get_networks(mode="bssid", signal_limit=50)
+            profiles = netshwlan.get_profiles()
+            network_set = set([n.name for n in networks])
+            profile_set = set([p.name for p in profiles])
+            intersect = network_set & profile_set
+            known_networks = []
+            for network in networks:
+                if network.name in intersect:
+                    networks.remove(network)
+                    known_networks.append(network)
+            line = get_line(known_networks + networks)
             print(line)
             ssid_log.write(line)
             ssid_log.flush()
