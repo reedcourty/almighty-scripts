53a3525434a07c0ac84389785d18ab44cc02455c
reedcourty <reedcourty@gmail.com>
2015-08-07 23:01:26 +0200

    Surrounded the main part with exception handling (and logging) and moved the logs to logs directory.

--- ap_logger/ap_logger.py
+++ ap_logger/ap_logger.py
@@ -19,13 +19,17 @@ if __name__ == '__main__':
     AP_LOGGER_PATH = os.path.dirname(os.path.abspath(__file__))
     
     SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
-    DEFAULT_LOG_FILE = os.path.join(AP_LOGGER_PATH, "{}-{}.log".format(SCRIPT_NAME, datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
+    DEFAULT_LOG_FILE = os.path.join(AP_LOGGER_PATH, "logs", "{}-{}.log".format(SCRIPT_NAME, datetime.datetime.now().strftime("%Y%m%d%H%M%S")))
     
     log_formatter = logging.Formatter("%(asctime)s -- %(levelname)s : %(name)s -- %(message)s")
     
-    file_handler = logging.FileHandler(DEFAULT_LOG_FILE)
-    file_handler.setFormatter(log_formatter)
-    file_handler.setLevel(logging.DEBUG)
+    try:
+        file_handler = logging.FileHandler(DEFAULT_LOG_FILE)
+        file_handler.setFormatter(log_formatter)
+        file_handler.setLevel(logging.DEBUG)
+    except FileNotFoundError:
+        os.makedirs(os.path.dirname(DEFAULT_LOG_FILE))
+    
     
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
@@ -36,21 +40,24 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    netshwlan = NetshWLAN()
+    try:
+        netshwlan = NetshWLAN()
 
-    while True:
-        with open(os.path.join(AP_LOGGER_PATH, "SSID.{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d"))), "a") as ssid_log:
-            networks = netshwlan.get_networks(mode="bssid", signal_limit=50)
-            profiles = netshwlan.get_profiles()
-            network_set = set([n.name for n in networks])
-            profile_set = set([p.name for p in profiles])
-            intersect = network_set & profile_set
-            known_networks = []
-            for network in networks:
-                if network.name in intersect:
-                    networks.remove(network)
-                    known_networks.append(network)
-            line = get_line(known_networks + networks)
-            ssid_log.write(line)
-            ssid_log.flush()
-        time.sleep(60)
+        while True:
+            with open(os.path.join(AP_LOGGER_PATH, "logs", "SSID.{}.log".format(datetime.datetime.now().strftime("%Y-%m-%d"))), "a") as ssid_log:
+                networks = netshwlan.get_networks(mode="bssid", signal_limit=50)
+                profiles = netshwlan.get_profiles()
+                network_set = set([n.name for n in networks])
+                profile_set = set([p.name for p in profiles])
+                intersect = network_set & profile_set
+                known_networks = []
+                for network in networks:
+                    if network.name in intersect:
+                        networks.remove(network)
+                        known_networks.append(network)
+                line = get_line(known_networks + networks)
+                ssid_log.write(line)
+                ssid_log.flush()
+            time.sleep(60)
+    except Exception as e:
+        logger.critical(e)
